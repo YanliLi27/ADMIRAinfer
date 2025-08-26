8803a276e721f0ea569dfed8d4e75f790e8e3319
yanlili <yanlili.cn@gmail.com>
2025-08-26 16:20:20 +0200

    create a look up table

---
+++ otherutils/projection_table.py
@@ -0,0 +1,109 @@
+import pandas as pd
+
+
+def get_projection(save:bool=False):
+    longitudinal:dict = [
+        # MT --------------------------------------------------------------
+        'MTD1ERO', 'MTD2ERO', 'MTD3ERO', 'MTD4ERO', 'MTD5ERO',  # MTERO - DE
+        'MTP1ERO',  'MTP2ERO', 'MTP3ERO', 'MTP4ERO', 'MTP5ERO', # MTERO - PE
+        
+        'MTD1BME', 'MTD2BME', 'MTD3BME', 'MTD4BME', 'MTD5BME',  # MTBME - D
+        'MTP1BME', 'MTP2BME', 'MTP3BME', 'MTP4BME', 'MTP5BME',  # MTBME - P
+        
+        'MT1SYN', 'MT2SYN', 'MT3SYN', 'MT4SYN',  'MT5SYN',  # MTSYN
+
+        'MT1ET', 'MT2ET', 'MT3ET', 'MT4ET', 'MT5ET',  # MTTSY - E
+        'MT1FT', 'MT2FT', 'MT3FT',  'MT4FT', 'MT5FT',  # MTTSY - F
+    
+        # MC --------------------------------------------------------------
+        'MCD2ERO', 'MCD3ERO', 'MCD4ERO', 'MCD5ERO',   # MCERO - D
+        'MCP2ERO', 'MCP3ERO', 'MCP4ERO', 'MCP5ERO',   # MCERO - P
+
+        'MCD2BME', 'MCD3BME', 'MCD4BME', 'MCD5BME',   # MCBME - D
+        'MCP2BME', 'MCP3BME', 'MCP4BME', 'MCP5BME',   # MCBME - P
+
+        'MC2SYN',  'MC3SYN', 'MC4SYN', 'MC5SYN',   # MCSYN
+        
+        'MC2ETS', 'MC3ETS', 'MC4ETS', 'MC5ETS', # MCTSY - E
+        'MC2FTS', 'MC3FTS', 'MC4FTS', 'MC5FTS', # MCTSY - F
+        
+        
+        # WR --------------------------------------------------------------
+        'WR1ERO', 'WR2ERO', 'WR3ERO', 'WR4ERO', 'WR5ERO',        # WRERO - D
+        'WRtmERO', 'WRtdERO', 'WRcaERO', 'WRhaERO', 'WRscERO',   # WRERO - TM, TD, CA, HA, SC
+        'WRluERO', 'WRtrERO', 'WRpiERO', 'WRraERO', 'WRulERO',   # WRERO - LU, TQ, PI, RA, UL
+        
+        
+        'WR1BME', 'WR2BME', 'WR3BME', 'WR4BME',  'WR5BME',       # WRBME
+        'WRtmBME', 'WRtdBME', 'WRcaBME', 'WRhaBME', 'WRscBME',   # WRBME - TM, TD, CA, HA, SC
+        'WRluBME', 'WRtrBME', 'WRpiBME', 'WRraBME', 'WRulBME',   # WRBME - LU, TQ, PI, RA, UL
+        
+        
+        'WRruSYN', 'WRrcSYN', 'WRicSYN',   # WRSYN
+    
+        'WRETSVI', 'WRETSV', 'WRETSIV', 'WRETSIII', 'WRETSII', 'WRETSI', # WRTSY
+        'WRFTS1', 'WRFTS2', 'WRFTS3', 'WRFTS4'                           # WRTSY
+        ]
+
+
+    previous = [
+        # MT --------------------------------------------------------------
+        'MTDERO1', 'MTDERO2', 'MTDERO3', 'MTDERO4', 'MTDERO5', # MTERO - DE
+        'MTPERO1', 'MTPERO2', 'MTPERO3', 'MTPERO4', 'MTPERO5', # MTERO - PE
+
+        'MTDBME1', 'MTDBME2', 'MTDBME3', 'MTDBME4', 'MTDBME5', # MTBME - D
+        'MTPBME1', 'MTPBME2', 'MTPBME3', 'MTPBME4', 'MTPBME5', # MTBME - P
+
+        'MTSYN1', 'MTSYN2', 'MTSYN3', 'MTSYN4', 'MTSYN5', # MTSYN
+
+        'MTETSY1', 'MTETSY2', 'MTETSY3', 'MTETSY4', 'MTETSY5',   # MTTSY - E
+        'MTFTSY1', 'MTFTSY2', 'MTFTSY3', 'MTFTSY4', 'MTFTSY5',    # MTTSY - F
+
+        # MC --------------------------------------------------------------
+        'MCDERO2', 'MCDERO3', 'MCDERO4', 'MCDERO5',   # MCERO - D
+        'MCPERO2', 'MCPERO3', 'MCPERO4', 'MCPERO5',   # MCERO - P
+
+        'MCDBME2', 'MCDBME3', 'MCDBME4', 'MCDBME5',   # MCBME - D
+        'MCPBME2', 'MCPBME3', 'MCPBME4', 'MCPBME5',   # MCBME - P
+
+        'MCSYN2', 'MCSYN3', 'MCSYN4', 'MCSYN5',    # MCSYN
+
+        'MCETSY2', 'MCETSY3', 'MCETSY4', 'MCETSY5',   # MCTSY - E
+        'MCFTSY2', 'MCFTSY3', 'MCFTSY4', 'MCFTSY5',   # MCTSY - F
+        
+
+        # WR --------------------------------------------------------------
+        'WRERO1', 'WRERO2', 'WRERO3', 'WRERO4', 'WRERO5',       # WRERO - D
+        'WREROTM', 'WREROTD', 'WREROCA', 'WREROHA', 'WREROSC',  # WRERO - TM, TD, CA, HA, SC
+        'WREROLU', 'WREROTQ', 'WREROPI', 'WRERORA', 'WREROUL',  # WRERO - LU, TQ, PI, RA, UL
+
+        'WRBME1', 'WRBME2', 'WRBME3', 'WRBME4', 'WRBME5',       # WRBME
+        'WRBMETM', 'WRBMETD', 'WRBMECA', 'WRBMEHA', 'WRBMESC',  # WRBME - TM, TD, CA, HA, SC
+        'WRBMELU', 'WRBMETQ', 'WRBMEPI', 'WRBMERA', 'WRBMEUL',  # WRBME - LU, TQ, PI, RA, UL
+            
+        'WRSYNRU', 'WRSYNRC', 'WRSYNIC',   # WRSYN
+
+        'WRTSYVI', 'WRTSYV', 'WRTSYIV', 'WRTSYIII', 'WRTSYII', 'WRTSYI',   # WRTSY
+        'WRTSY1', 'WRTSY2', 'WRTSY3', 'WRTSY4'                            # WRTSY
+    ]
+
+    # WR1 = [item for item in longitudinal if 'WR' in item[:2]]
+    # WR2 = [item for item in previous if 'WR' in item[:2]]
+    # MC1 = [item for item in longitudinal if 'MC' in item[:2]]
+    # MC2 = [item for item in previous if 'MC' in item[:2]]
+    # MT1 = [item for item in longitudinal if 'MT' in item[:2]]
+    # MT2 = [item for item in previous if 'MT' in item[:2]]
+
+    if save:
+        df = pd.DataFrame({'longitudinal':longitudinal , 'previous':previous})
+        df.to_csv(r"Projection_table.csv")
+
+    projection:dict = {}
+    for item in zip(*[longitudinal, previous]):
+        projection[item[0]] = item[1]
+    
+    return projection
+
+
+if __name__=="__main__":
+    get_projection()
--- otherutils/sav2csv.py
+++ otherutils/sav2csv.py
@@ -2,6 +2,8 @@ import pandas as pd
 import pyreadstat
 import re
 from collections import defaultdict
+from projection_table import get_projection
+
 
 def group_anagrams(words):
     groups = defaultdict(list)
@@ -16,34 +18,10 @@ def find_anagrams(s, words):
     return [w for w in words if ''.join(sorted(w)) == key and w != s]
 
 if __name__=="__main__":
-    project:dict = \
-    {
-    'WRLUERO':'WREROLU', 
-    'WRULERO':'WREROUL', 
-    'WRLUBME':'WRBMELU', 
-    'WRULBME':'WRBMEUL', 
-    'WRVITSY':'WRTSYVI', 
-    'WRIVTSY':'WRTSYIV'
-    }
-
-    lib:list = \
-    [
-    'WRERO1', 'WRERO2', 'WRERO3', 'WRERO4', 'WRERO5', 'WREROTM', 'WREROTD', 'WREROCA', 'WREROHA', 'WREROSC', 
-    'WREROLU', 'WREROTQ', 'WREROPI', 'WRERORA', 'WREROUL', 
-    'WRBME1', 'WRBME2', 'WRBME3', 'WRBME4', 'WRBME5', 'WRBMETM', 'WRBMETD', 'WRBMECA', 
-    'WRBMEHA', 'WRBMESC', 'WRBMELU', 'WRBMETQ', 'WRBMEPI', 'WRBMERA', 'WRBMEUL', 
-    'WRSYNRU', 'WRSYNRC', 'WRSYNIC', 
-    'WRTSYVI', 'WRTSYV', 'WRTSYIV', 'WRTSYIII', 'WRTSYII', 'WRTSYI', 'WRTSY1', 'WRTSY2', 'WRTSY3', 'WRTSY4',  # wrist
-    'MCDERO2', 'MCDERO3', 'MCDERO4', 'MCDERO5', 'MCPERO2', 'MCPERO3', 'MCPERO4', 'MCPERO5', 
-    'MCDBME2', 'MCDBME3', 'MCDBME4', 'MCDBME5', 'MCPBME2', 'MCPBME3', 'MCPBME4', 'MCPBME5', 
-    'MCSYN2', 'MCSYN3', 'MCSYN4', 'MCSYN5', 
-    'MCFTSY2', 'MCFTSY3', 'MCFTSY4', 'MCFTSY5', 'MCETSY2', 'MCETSY3', 'MCETSY4', 'MCETSY5',  # mcp
-    'MTDERO1', 'MTDERO2', 'MTDERO3', 'MTDERO4', 'MTDERO5', 'MTPERO1', 'MTPERO2', 'MTPERO3', 'MTPERO4', 'MTPERO5', # MTERO
-    'MTDBME1', 'MTDBME2', 'MTDBME3', 'MTDBME4', 'MTDBME5', 'MTPBME1', 'MTPBME2', 'MTPBME3', 'MTPBME4', 'MTPBME5', # MTBME
-    'MTSYN1', 'MTSYN2', 'MTSYN3', 'MTSYN4', 'MTSYN5', # MTSYN
-    'MTETSY1', 'MTETSY2', 'MTETSY3', 'MTETSY4', 'MTETSY5', 'MTFTSY1', 'MTFTSY2', 'MTFTSY3', 'MTFTSY4', 'MTFTSY5'  # MTTSY 
-    ]
-
+    projection:dict = get_projection(False)
+    projection_cnt:dict = {}
+    for key in projection.keys():
+        projection_cnt[key] = 1
     # 读取sav文件
     path_lists = [r'R:\ESMIRA\ESMIRA_Scores\SPSS data\5. CSA_T1_MRI_scores_SPSS.sav',
                 r'R:\ESMIRA\ESMIRA_Scores\SPSS data\6. CSA_T2_MRI_scores_SPSS.sav',
@@ -62,16 +40,12 @@ if __name__=="__main__":
         if idx==4:
             new_columns = []
             for col in df.columns:
-                if col in ['WRLUERO', 'WRULERO', 'WRLUBME', 'WRULBME', 'WRVITSY', 'WRIVTSY']:
-                    new_columns.append(project[col])
+                if col in projection.keys():
+                    new_columns.append(projection[col])
+                    projection_cnt[col] -= 1
                 else:
-                    new_cols:list = find_anagrams(col, lib)
-                    if len(new_cols)==1:
-                        new_columns.append(new_cols[0])
-                    elif len(new_cols)>1:
-                        raise ValueError(f'{new_cols} are anagrams for {col}')
-                    else:
-                        new_columns.append(col)  # 如果没有匹配，保持不变
+                    new_columns.append(col)  # 如果没有匹配，保持不变
+            assert sum(projection_cnt.values())==0
             # 替换列名
             df.columns = new_columns
 
